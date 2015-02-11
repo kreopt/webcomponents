5e67295b2fdcbde7bd6195aa9961ddd175dc67c8
kreopt_wf34 <kreopt@gmail.com>
2015-02-11 18:58:54 +0600

    spinner & fixes

--- sf-action-button/sf-action-button.js
+++ sf-action-button/sf-action-button.js
@@ -1,65 +1,62 @@
 !function () {
-    var owner = (document.currentScript || {}).ownerDocument || document;
-    $(owner).ready(function(){
-        var sfActionButton = Object.create(HTMLButtonElement.prototype, {
-            action: {value: "", writable: true}
-        });
+    var sfActionButton = Object.create(HTMLButtonElement.prototype, {
+        action: {value: "", writable: true}
+    });
 
-        sfActionButton.createdCallback = function () {
-            this.addEventListener('click', function(e){
-                var url = this.getAttribute('action') || "";
-                var _this = this;
-                _this.setAttribute('disabled', 'disabled');
-                $.ajax({
-                    url: url,
-                    success: function(r){
-                        var evt = new CustomEvent("success", {detail:{response:r}});
-                        _this.dispatchEvent(evt);
-                        _this.removeAttribute('disabled');
-                    },
-                    error: function(e){
-                        var evt = new CustomEvent("error", {detail:{error:e}});
-                        _this.dispatchEvent(evt);
-                        _this.removeAttribute('disabled');
-                    }
-                })
-            }, false);
-        };
+    sfActionButton.createdCallback = function () {
+        this.addEventListener('click', function (e) {
+            var url = this.getAttribute('action') || "";
+            var _this = this;
+            _this.setAttribute('disabled', 'disabled');
+            $.ajax({
+                url: url,
+                success: function (r) {
+                    var evt = new CustomEvent("success", {detail: {response: r}});
+                    _this.dispatchEvent(evt);
+                    _this.removeAttribute('disabled');
+                },
+                error: function (e) {
+                    var evt = new CustomEvent("error", {detail: {error: e}});
+                    _this.dispatchEvent(evt);
+                    _this.removeAttribute('disabled');
+                }
+            })
+        }, false);
+    };
 
-        document.registerElement('sf-action-button', {
-            prototype: sfActionButton,
-            extends:'button'
-        });
+    document.registerElement('sf-action-button', {
+        prototype: sfActionButton,
+        extends: 'button'
+    });
 
-        var sfActionCb = Object.create(HTMLInputElement.prototype);
+    var sfActionCb = Object.create(HTMLInputElement.prototype);
 
-        sfActionCb.createdCallback = function () {
-            this.addEventListener('change', function(e){
-                var url = this.getAttribute('action') || "";
-                var _this = this;
-                _this.setAttribute('disabled', 'disabled');
-                $.ajax({
-                    url: url,
-                    data:{
-                        checked:_this.checked?1:''
-                    },
-                    success: function(r){
-                        var evt = new CustomEvent("success", {detail:{response:r}});
-                        _this.dispatchEvent(evt);
-                        _this.removeAttribute('disabled');
-                    },
-                    error: function(e){
-                        var evt = new CustomEvent("error", {detail:{error:e}});
-                        _this.dispatchEvent(evt);
-                        _this.removeAttribute('disabled');
-                    }
-                })
-            }, false);
-        };
+    sfActionCb.createdCallback = function () {
+        this.addEventListener('change', function (e) {
+            var url = this.getAttribute('action') || "";
+            var _this = this;
+            _this.setAttribute('disabled', 'disabled');
+            $.ajax({
+                url: url,
+                data: {
+                    checked: _this.checked ? 1 : ''
+                },
+                success: function (r) {
+                    var evt = new CustomEvent("success", {detail: {response: r}});
+                    _this.dispatchEvent(evt);
+                    _this.removeAttribute('disabled');
+                },
+                error: function (e) {
+                    var evt = new CustomEvent("error", {detail: {error: e}});
+                    _this.dispatchEvent(evt);
+                    _this.removeAttribute('disabled');
+                }
+            })
+        }, false);
+    };
 
-        document.registerElement('sf-action-checkbox', {
-            prototype: sfActionCb,
-            extends:'input'
-        });
+    document.registerElement('sf-action-checkbox', {
+        prototype: sfActionCb,
+        extends: 'input'
     });
 }();
--- sf-ajax-form/sf-ajax-form.js
+++ sf-ajax-form/sf-ajax-form.js
@@ -1,82 +1,80 @@
 !function () {
-    var owner = (document.currentScript || {}).ownerDocument || document;
-    $(owner).ready(function(){
-        var sfAjaxFormProto = Object.create(HTMLFormElement.prototype, {});
 
-        sfAjaxFormProto.submit = function (e) {
-            e.preventDefault();
-            if (!this.checkValidity()){
-                return
-            }
+    var sfAjaxFormProto = Object.create(HTMLFormElement.prototype, {});
 
-            var fd = new FormData(this);
-            var url = this.getAttribute('action') || "";
-            var method = this.getAttribute('method') || "POST";
-            var data_type = this.getAttribute('type') || "text";
-            var _this = this;
-            var buttons = this.getElementsByTagName('button');
-            buttons.forEach(function(e){
-                e.setAttribute('disabled', 'disabled');
-            });
-            $.ajax({
-                url: url,
-                type:method,
-                data: fd,
-                dataType: data_type,
-                processData: false,
-                success: function(r){
-                    var evt = new CustomEvent("success", {detail:{response:r}});
-                    _this.dispatchEvent(evt);
-                    buttons.forEach(function(e){
-                        e.removeAttribute('disabled');
-                    });
-                },
-                error: function(e){
-                    var evt = new CustomEvent("error", {detail:{error:e}});
-                    _this.dispatchEvent(evt);
-                    buttons.forEach(function(e){
-                        e.removeAttribute('disabled');
-                    });
-                }
-            })
-        };
+    sfAjaxFormProto.submit = function (e) {
+        e.preventDefault();
+        if (!this.checkValidity()) {
+            return
+        }
 
-        sfAjaxFormProto.createdCallback = function () {
-            console.log('created');
-            //var t = owner.querySelector('#ajax_form_template');
-            //var clone = owner.importNode(t.content, true);
-            //var shadow = this.createShadowRoot();
-            //shadow.appendChild(clone);
-            //this.addEventListener('click', function(e){
-            //    var el = e.target;
-            //    if (el && el.tagName == 'BUTTON' && el.type=="submit") {
-            //        var evt = new Event("submit");
-            //        this.dispatchEvent(evt);
-            //    }
-            //
-            //}, false);
-            this.addEventListener('submit',this.submit, false);
-        };
-
-        sfAjaxFormProto.attachedCallback = function () {
-            console.log('attached');
-        };
-
-        sfAjaxFormProto.detachedCallback = function () {
-            console.log('detached');
-        };
-
-        sfAjaxFormProto.attributeChangedCallback = function (name, oldVal, newVal) {
-            console.log(
-                'Attribute ' + name +
-                ' changed from ' + oldVal +
-                ' to ' + newVal + '\n');
-        };
-
-
-        document.registerElement('sf-ajax-form', {
-            prototype: sfAjaxFormProto,
-            extends:"form"
+        var fd = new FormData(this);
+        var url = this.getAttribute('action') || "";
+        var method = this.getAttribute('method') || "POST";
+        var data_type = this.getAttribute('type') || "text";
+        var _this = this;
+        var buttons = this.getElementsByTagName('button');
+        buttons.forEach(function (e) {
+            e.setAttribute('disabled', 'disabled');
         });
+        $.ajax({
+            url: url,
+            type: method,
+            data: fd,
+            dataType: data_type,
+            processData: false,
+            success: function (r) {
+                var evt = new CustomEvent("success", {detail: {response: r}});
+                _this.dispatchEvent(evt);
+                buttons.forEach(function (e) {
+                    e.removeAttribute('disabled');
+                });
+            },
+            error: function (e) {
+                var evt = new CustomEvent("error", {detail: {error: e}});
+                _this.dispatchEvent(evt);
+                buttons.forEach(function (e) {
+                    e.removeAttribute('disabled');
+                });
+            }
+        })
+    };
+
+    sfAjaxFormProto.createdCallback = function () {
+        console.log('created');
+        //var t = owner.querySelector('#ajax_form_template');
+        //var clone = owner.importNode(t.content, true);
+        //var shadow = this.createShadowRoot();
+        //shadow.appendChild(clone);
+        //this.addEventListener('click', function(e){
+        //    var el = e.target;
+        //    if (el && el.tagName == 'BUTTON' && el.type=="submit") {
+        //        var evt = new Event("submit");
+        //        this.dispatchEvent(evt);
+        //    }
+        //
+        //}, false);
+        this.addEventListener('submit', this.submit, false);
+    };
+
+    sfAjaxFormProto.attachedCallback = function () {
+        console.log('attached');
+    };
+
+    sfAjaxFormProto.detachedCallback = function () {
+        console.log('detached');
+    };
+
+    sfAjaxFormProto.attributeChangedCallback = function (name, oldVal, newVal) {
+        console.log(
+            'Attribute ' + name +
+            ' changed from ' + oldVal +
+            ' to ' + newVal + '\n');
+    };
+
+
+    document.registerElement('sf-ajax-form', {
+        prototype: sfAjaxFormProto,
+        extends: "form"
     });
 }();
